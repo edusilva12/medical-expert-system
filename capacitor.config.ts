b84afbde8c93d8b2e6fe61ef5fb15afe91a5698c
Eduardo Silva <silvaeduardoe23@gmail.com>
2023-12-08 21:40:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'medical-expert-system',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
