fff9f4d025bc579c80182c8414a3842987b73e4a
Raisa Chantal <raisae06@gmail.com>
2023-12-10 09:05:24 -0500

    add: base de conocimiento y enlace a la interfaz

---
+++ src/app/home/data.ts
@@ -0,0 +1,62 @@
+let cholesterol: Array<string> = [
+  '¿Tiene vision borrosa?',
+  '¿Tiene hinchazon en alguna extremidad?',
+  '¿Tiene perdida del equilibrio?',
+  '¿Tiene dolor de cabeza?',
+  '¿Tiene amarillos los ojos?',
+  '¿Tiene adormecimiento en alguna extremidad?',
+  '¿Tiene agitacion,en especial al caminar o al realizar actividades leve?',
+  '¿Tiene dolor en el pecho?',
+  'Tiene Colesterol alto',
+];
+
+let diabetics: Array<string> = [
+  '¿Tiene vision borrosa?',
+  '¿Padece de orina frecuente?',
+  '¿Tiene sed constante?',
+  '¿Tiene hambre excesiva?',
+  '¿Tiene perdida de peso inexplicable?',
+  '¿Se siente fatigado?',
+  '¿Tiene irritabilidad?',
+  'Tiene Diabetes',
+];
+
+let ebola: Array<string> = [
+  '¿Tiene fiebre?',
+  '¿Presenta dolores musculares?',
+  '¿Tiene vómito y diarrea?',
+  '¿Presenta erupciones cutaneas?',
+  '¿Siente debilidad intensa?',
+  '¿Tiene dolor de garganta?',
+  'Tiene Ebola',
+];
+
+let gastritis: Array<string> = [
+  '¿Tiene dolor abdominal?',
+  '¿Tiene acidez estomacal?',
+  '¿Presenta aerofagia?',
+  '¿Tiene ausencia de hambre que en ocasiones puede producir perdida de peso?',
+  '¿Presenta heces de color negro o con sangrado?',
+  '¿Tiene náuseas?',
+  'Tiene Gastritis',
+];
+
+let pneumonia: Array<string> = [
+  '¿Tiene fiebre?',
+  '¿Tiene dolores articulares?',
+  '¿Ha tenido tos constante los ultimos dos dias?',
+  '¿Presenta dificultad para respirar?',
+  'Tiene Neumonia',
+];
+
+let parkinson: Array<string> = [
+  '¿Presenta temblor en alguna de las extremidades superiores del cuerpo?',
+  '¿Tiene dolores articulares?',
+  '¿Ha notado algún cambio perdida de movimiento espontáneo y automático en alguna extremidad?',
+  '¿Ha presentado rigidez severa en alguna region muscular?',
+  '¿Sufre de depresión o ha utilizado farmacos para tratar una enfermedad semejante?',
+  '¿Presenta algun trastorno en el sueño?',
+  'Tiene Parkinson',
+];
+
+export { cholesterol, diabetics, ebola, pneumonia, parkinson, gastritis };
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
 import { refreshCircle, refreshCircleSharp } from 'ionicons/icons';
+import {
+  cholesterol,
+  diabetics,
+  ebola,
+  gastritis,
+  parkinson,
+  pneumonia,
+} from './data';
 
 @Component({
   selector: 'app-home',
@@ -16,14 +24,101 @@ export class HomePage implements OnInit {
   showCard = false;
   showButtonStarted = true;
 
+  // new code
+  cardQuestions: boolean = false;
+  cardAnswer: boolean = false;
+  buttonInitial: boolean = true;
+  questionText: string = '';
+  answerText: string = '';
+  counter: number = 0;
+  counterQuestionsList: number = 0;
+
   constructor() {
     addIcons({ refreshCircle, refreshCircleSharp });
   }
 
   ngOnInit() {}
 
+  reset() {
+    this.counter = 0;
+    this.counterQuestionsList = 0;
+    this.questionText = '';
+    this.answerText = '';
+    this.cardAnswer = false;
+    this.cardQuestions = false;
+    this.buttonInitial = true;
+  }
+
+  setCardQuestions() {
+    this.cardQuestions = !this.cardQuestions;
+  }
+
+  setCardAnswer() {
+    this.cardAnswer = !this.cardAnswer;
+  }
+
+  setButtonInitial() {
+    this.buttonInitial = !this.buttonInitial;
+  }
+
+  validateAnswer(questionsList: Array<string>) {
+    if (this.counter < questionsList.length) {
+      this.questionText = questionsList[this.counter];
+      if (this.counter + 1 == questionsList.length) {
+        this.questionText = questionsList[this.counter - 1];
+        this.answerText = questionsList[this.counter];
+        this.setCardQuestions();
+        this.setCardAnswer();
+      }
+      this.counter++;
+    }
+  }
+
+  question(answer: boolean) {
+    if (!answer) {
+      this.counterQuestionsList++;
+      this.counter = 0;
+      this.question(true);
+    }
+    if (answer) {
+      switch (this.counterQuestionsList) {
+        case 0:
+          this.validateAnswer(cholesterol);
+          break;
+        case 1:
+          this.validateAnswer(diabetics);
+          break;
+        case 2:
+          this.validateAnswer(ebola);
+          break;
+        case 3:
+          this.validateAnswer(pneumonia);
+          break;
+        case 4:
+          this.validateAnswer(gastritis);
+          break;
+        case 5:
+          this.validateAnswer(parkinson);
+          break;
+        default:
+          {
+            this.cardQuestions = false;
+            this.cardAnswer = true;
+            this.answerText = 'No estoy entrenado para darte ese diagnostico';
+          }
+          break;
+      }
+    }
+  }
+
+  // old code
   toggleCard() {
     this.showCard = !this.showCard;
     this.showButtonStarted = !this.showButtonStarted;
+  }
+
+  resetBtnCard() {
+    this.showCard = false;
+    this.showButtonStarted = true;
   }
 }
